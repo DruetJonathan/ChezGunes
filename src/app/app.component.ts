f555b8ecfb6ab79044cdd006aefd73566888d3d7
Jonathan Druet <jonathandruet2@gmail.com>
2023-10-09 00:02:37 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,99 +1,26 @@
-import {Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild} from '@angular/core';
-import * as Hammer from 'hammerjs';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit  {
 
   ////////////////////////////////////////////////////////////////////////////////////
   constructor(private renderer: Renderer2) {
   }
-  @ViewChild('imgContainer') imgContainer!: ElementRef;
   ngOnInit(): void {
-    // gallery.ts
-    const hammer = new Hammer(this.imgContainer.nativeElement);
-    hammer.get('doubletap').recognizeWith('pan');
-    hammer.on('doubletap', (e: HammerInput) => {
-      // Gestionnaire d'événement doubletap (peut être vide)
+    // @ts-ignore
+    document.querySelector("#imgContainer").addEventListener("wheel", function(event: WheelEvent) {
+      event.preventDefault();
+      // Déplacer la div de event.deltaX pixels en X
+      const element =  document.querySelector("#imgContainer")
+      if (element){
+        element.scrollLeft -= event.deltaY*4;
+        console.log(event.deltaX)
+      }
     });
 
-    let isDragging = false;
-    let startX: number;
-    let scrollLeft: number;
-
-    hammer.on('panstart', (e: HammerInput) => {
-      isDragging = true;
-      startX = e.center.x;
-      scrollLeft = this.imgContainer.nativeElement.scrollLeft;
-      this.imgContainer.nativeElement.style.cursor = 'grabbing';
-    });
-
-    hammer.on('panmove', (e: HammerInput) => {
-      if (!isDragging) return;
-      const x = e.center.x;
-      const walk = (x - startX) * 2;
-      this.imgContainer.nativeElement.scrollLeft = scrollLeft - walk;
-    });
-
-    hammer.on('panend', () => {
-      isDragging = false;
-      this.imgContainer.nativeElement.style.cursor = 'grab';
-    });
-
-// Attendre que le DOM soit prêt
-//     document.addEventListener("DOMContentLoaded", () => {
-//       // Sélectionnez votre conteneur d'images
-//       const imgContainer = document.getElementById("imgContainer");
-//       const makeyframe = document.querySelector(".fade-in");
-//       let isDragging = false;
-//       let startX: number;
-//       let scrollLeft: number;
-//
-//
-//       const startDrag = (e: Event) => {
-//         e.preventDefault();
-//         isDragging = true;
-//         const event = e instanceof MouseEvent ? e : (e as TouchEvent).touches[0];
-//         startX = event.pageX - (imgContainer?.offsetLeft || 0);
-//         scrollLeft = imgContainer?.scrollLeft || 0;
-//         imgContainer?.style.setProperty("cursor", "grabbing");
-//       };
-//
-//       const handleDrag = (e: Event) => {
-//         if (!isDragging) return;
-//         e.preventDefault();
-//         const event = e instanceof MouseEvent ? e : (e as TouchEvent).touches[0];
-//         const x = event.pageX - (imgContainer?.offsetLeft || 0);
-//         const walk = (x - startX) * 2; // Ajustez le facteur pour contrôler la vitesse de défilement
-//         if (imgContainer) {
-//           imgContainer.scrollLeft = scrollLeft - walk;
-//         }
-//       };
-//
-//       const stopDrag = () => {
-//         isDragging = false;
-//         imgContainer?.style.setProperty("cursor", "grab");
-//       };
-//
-//       imgContainer?.addEventListener("mousedown", startDrag);
-//       imgContainer?.addEventListener("mousemove", handleDrag);
-//       imgContainer?.addEventListener("mouseup", stopDrag);
-//
-//       imgContainer?.addEventListener("touchstart", startDrag);
-//       imgContainer?.addEventListener("touchmove", handleDrag);
-//       imgContainer?.addEventListener("touchend", stopDrag);
-//
-//
-//       imgContainer?.addEventListener("mousedown", startDrag);
-//       imgContainer?.addEventListener("touchstart", startDrag);
-//       document.addEventListener("mousemove", handleDrag);
-//       document.addEventListener("touchmove", handleDrag, {passive: false});
-//       document.addEventListener("mouseup", stopDrag);
-//       document.addEventListener("touchend", stopDrag);
-//       document.addEventListener("mouseleave", stopDrag);
-//     });
     const scrollToTopButton = document.getElementById("scrollToTopButton");
 
 // Ajoutez un gestionnaire d'événements pour faire défiler vers le haut lorsque le bouton est cliqué
